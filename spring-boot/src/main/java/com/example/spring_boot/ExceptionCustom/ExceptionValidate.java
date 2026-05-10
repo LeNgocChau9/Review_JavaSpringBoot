b61c2f0848bb5190d4341914919f73a91540004b
Lê Ngọc Châu <lengocchau15072006@gmail.com>
2026-05-10 21:59:42 +0700

    chỉnh sửa lại cấu trúc thư mục

--- spring-boot/src/main/java/com/example/spring_boot/ExceptionCustom/ExceptionValidate.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_boot.ExceptionCustom;
-
-public class ExceptionValidate extends Exception {
-    public ExceptionValidate(String message){
-        super(message);
-    }
-}
