62b7c22906862825449d30b8e8064f93b984ccad
Lê Ngọc Châu <lengocchau15072006@gmail.com>
2026-05-10 21:46:57 +0700

    ôn lại cách nhận request và trả về respone ý nghĩa các anotation cơ bản

---
+++ spring-boot/src/main/java/com/example/spring_boot/DTO/AddRoomBean.java
@@ -0,0 +1,12 @@
+package com.example.spring_boot.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddRoomBean {
+    private String name;
+    private String addrress;
+    private Integer Age;
+}
---
+++ spring-boot/src/main/java/com/example/spring_boot/ExceptionCustom/ExceptionValidate.java
@@ -0,0 +1,7 @@
+package com.example.spring_boot.ExceptionCustom;
+
+public class ExceptionValidate extends Exception {
+    public ExceptionValidate(String message){
+        super(message);
+    }
+}
